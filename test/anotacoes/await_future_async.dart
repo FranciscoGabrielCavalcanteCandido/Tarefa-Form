6b7993f7cac23abe7b3ab1e5e1cb005b01ba82f8
fg.14.cavalcandido@gmail.com <fg.14.cavalcandido@gmail.com>
2022-07-12 20:54:48 -0300

    formulario de tarefas

---
+++ test/anotacoes/await_future_async.dart
@@ -0,0 +1,14 @@
+//O async determina que um método será assíncrono, ou seja, não irá retornar algo
+// imediatamente, então o aplicativo pode continuar a execução de outras tarefas 
+//enquanto o processamento não é finalizado.
+
+
+
+//O await serve para determinar que o aplicativo deve esperar uma resposta de uma
+// função antes de continuar a execução. Isso é muito importante pois há casos em
+// que uma função depende do retorno de outra.
+
+
+
+//Já o Future determina que uma função irá retornar algo no “futuro”, ou seja,
+// é uma função que levará um tempo até ser finalizada.
